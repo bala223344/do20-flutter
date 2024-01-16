af1201dc193f6ca32e06139b4b7e52d279ab4e85
press <bala223344@gmail.com>
2024-01-16 12:42:04 +0530

    signup shown

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,15 @@
 // Copyright 2022 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
+import 'package:firebase_auth/firebase_auth.dart' // new
+    hide EmailAuthProvider, PhoneAuthProvider;    // new
+import 'package:flutter/material.dart';           // new
+import 'package:provider/provider.dart';          // new
 import 'package:flutter/material.dart';
 
 import 'src/widgets.dart';
+import 'app_state.dart';                          // new
+import 'src/authentication.dart';                 // new
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -21,6 +26,13 @@ class HomePage extends StatelessWidget {
           const SizedBox(height: 8),
           const IconAndDetail(Icons.calendar_today, 'October 30'),
           const IconAndDetail(Icons.location_city, 'San Francisco'),
+           Consumer<ApplicationState>(
+            builder: (context, appState, _) => AuthFunc(
+                loggedIn: appState.loggedIn,
+                signOut: () {
+                  FirebaseAuth.instance.signOut();
+                }),
+          ),
           const Divider(
             height: 8,
             thickness: 1,

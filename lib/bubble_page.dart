67e89dcc6d0f43fbcc7dc954ec24eee74476f3e8
press <bala223344@gmail.com>
2024-04-19 10:01:35 +0530

    dialog

--- lib/bubble_page.dart
+++ lib/bubble_page.dart
@@ -37,7 +37,7 @@ class _BubblePageState extends State<BubblePage> {
   int _start = 10;
 
   void startTimer() {
-    const oneSec =  Duration(seconds: 1);
+    const oneSec = Duration(seconds: 1);
     _timer = Timer.periodic(
       oneSec,
       (Timer timer) {
@@ -54,81 +54,105 @@ class _BubblePageState extends State<BubblePage> {
     );
   }
 
+  void showDialog() {
+
+    showGeneralDialog(
+        context: context,
+        barrierDismissible: true,
+        barrierLabel:
+            MaterialLocalizations.of(context).modalBarrierDismissLabel,
+        barrierColor: Colors.black45,
+        transitionDuration: const Duration(milliseconds: 200),
+        pageBuilder: (BuildContext buildContext, Animation animation,
+            Animation secondaryAnimation) {
+          return Center(
+            child: Container(
+              width: MediaQuery.of(context).size.width - 10,
+              height: MediaQuery.of(context).size.height - 80,
+              padding: EdgeInsets.all(20),
+              color: Colors.white,
+              child: Column(
+                children: [
+                  Center(
+                      child: Container(
+                          child: Text(
+                          'reminder to put device on silent! ',
+                    // style: TextStyle(color: const Color.fromARGB(255, 14, 0, 0)),
+                  ))),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text(
+                      "Save",
+                      // style: TextStyle(color: Colors.white),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Sample Code'),
       ),
-      body: 
-      Column(
-        children: [
-          Expanded(
-            child: ListView(
-
-              children: <Widget>[
-                
-                SizedBox(height: 26.0),
-                Center(child: Text('You have pressed the button  times.')),
-
-                          Align(
-                  
-                  child: Container(
-                    height: 128,
-                    width: 128,
-                    decoration:
-                        BoxDecoration(shape: BoxShape.circle, boxShadow: [
-                      BoxShadow(
-                        color: Color(0xFF9B5DE5),
-                        blurRadius: 2.0,
-                        spreadRadius: -5.0,
-                        offset: Offset(0.0, 8.0),
-                      ),
-                    ]),
-                      child: WaterDrop(
-                      params: [
-                        WaterDropParam(
-                            top: 0, height: 128, left: 0, width: 128),
-                      ],
-                      child: ClipOval(
-                        child: WaveWidget(
-                          config: CustomConfig(
-                            colors: [
-                              Color.fromARGB(255, 140, 189, 211),
-                              Color.fromARGB(255, 18, 38, 57),
-                              
-                            ],
-                            durations: [
-                              5000,
-                              4000,
-                            ],
-                            heightPercentages: [
-                              _wav1,
-                              _wav2,
-                            ],
-                          ),
-                          backgroundColor: Color.fromARGB(255, 255, 151, 6),
-                          size: Size(double.infinity, double.infinity),
-                          waveAmplitude: 0,
-                        ),
-                      ),
-                      ),
-                    ),
-                  
+      body: Column(children: [
+        Expanded(
+            child: ListView(children: <Widget>[
+          SizedBox(height: 26.0),
+          Center(child: Text('You have pressed the button $_start  times.')),
+          Align(
+            child: Container(
+              height: 128,
+              width: 128,
+              decoration: BoxDecoration(shape: BoxShape.circle, boxShadow: [
+                BoxShadow(
+                  color: Color(0xFF9B5DE5),
+                  blurRadius: 2.0,
+                  spreadRadius: -5.0,
+                  offset: Offset(0.0, 8.0),
                 ),
-              ]
-            )
-          )
-          ]
-      ),
-
-      
+              ]),
+              child: WaterDrop(
+                params: [
+                  WaterDropParam(top: 0, height: 128, left: 0, width: 128),
+                ],
+                child: ClipOval(
+                  child: WaveWidget(
+                    config: CustomConfig(
+                      colors: [
+                        Color.fromARGB(255, 140, 189, 211),
+                        Color.fromARGB(255, 18, 38, 57),
+                      ],
+                      durations: [
+                        5000,
+                        4000,
+                      ],
+                      heightPercentages: [
+                        _wav1,
+                        _wav2,
+                      ],
+                    ),
+                    backgroundColor: Color.fromARGB(255, 255, 151, 6),
+                    size: Size(double.infinity, double.infinity),
+                    waveAmplitude: 0,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ]))
+      ]),
       floatingActionButton: FloatingActionButton(
-        onPressed: startTimer,
+        onPressed: showDialog,
         tooltip: 'main.start_20_minutes_bubble'.tr(),
         child: const Icon(Icons.add),
       ),
-      
     );
 
     return Container(
@@ -136,12 +160,11 @@ class _BubblePageState extends State<BubblePage> {
         children: <Widget>[
           if (_showText) Text('Now you see me!'),
           ElevatedButton(
-       //     onPressed: _toggleText,
+            //     onPressed: _toggleText,
             onPressed: startTimer,
             child: Text('$_start'),
           ),
         ],
-        
       ),
     );
   }
